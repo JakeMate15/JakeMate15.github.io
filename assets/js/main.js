f3113f985a46cf8349af6bb2ff828450a9f48fc1
JakeMate15 <erik.mofe51@gmail.com>
2024-03-25 16:59:52 -0600

    correciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,16 @@ let i = 1;
 const images = 9;
 const hero = document.querySelector('.hero');
 
-const colors = ['#DCE1FF', '#E1F0F4', '#d3d2d8', '#b7d4e3', '#b5cfce', '#f0e8da', '#e3c4c5', '#d2d8eb', '#c6c8d0']
+const colors = ['#DCE1FF', '#E1F0F4', '#d3d2d8', '#b7d4e3', '#b5cfce', '#f0e8da', '#e3c4c5', '#d2d8eb', '#c6c8d0'];
 
-setInterval(() => {
+function updateHeroBackground() {
   hero.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('assets/img/background (${i}).jpg')`;
-  
-  document.documentElement.style.setProperty('--Material-Blue-Light', colors[i-1]);
-  console.log(colors[i-1]);
+  document.documentElement.style.setProperty('--Material-Blue-Light', colors[i - 1]);
+  console.log(colors[i - 1]);
   console.log(i);
-
   i = (i % images) + 1;
+}
 
-}, 6000);
+updateHeroBackground();
+
+setInterval(updateHeroBackground, 6000);
